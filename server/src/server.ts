025b0a60696f0264c16365af2a4ad0aef1442577
Zehan Khan <zkhan@hootboard.com>
2023-10-19 21:11:33 +0530

    build: :package: add required packages

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
 import app from "@app";
 import { Server } from "http";
-import { Bugatti } from "misc/emoji";
 
 const PORT = 2000;
 
 const server: Server = app.listen(PORT, (): void => {
-  console.log(`${Bugatti}
-  ⠀⚡️[server]: is running fast as Bugatti on http://localhost:${PORT}`);
+  console.log(`⚡️[server]: is running fast as Bugatti on http://localhost:${PORT}`);
 });
